811c1d6be697d817b4816691443da768e285fa2b
catarock8 <catarock822@gmail.com>
2024-10-20 22:06:45 -0300

    Creacion paginas en templates
    Se crearon las paginas y se agregaron en views.py y urls.py

--- main/urls.py
+++ main/urls.py
@@ -2,5 +2,29 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='home'),
+    #urls para cliente
+    path('', views.home_cliente, name='home_cliente'),
+    path('capacitaciones/cliente/', views.capacitaciones_cliente, name='capacitaciones_cliente'),
+    path('mis_evaluaciones/cliente/', views.mis_evaluaciones_cliente, name='mis_evaluaciones_cliente'),
+    path('visitas_medicas/cliente/', views.visitas_medicas_cliente, name='visitas_medicas_cliente'),
+    #urls para ingeniero
+    path('home/ingeniero/', views.home_ingeniero, name='home_ingeniero'),
+    path('capacitaciones/ingeniero/', views.capacitaciones_ingeniero, name='capacitaciones_ingeniero'),
+    path('evaluaciones/ingeniero/', views.evaluaciones_ingeniero, name='evaluaciones_ingeniero'),
+    path('visitas_medicas/ingeniero/', views.visitas_medicas_ingeniero, name='visitas_medicas_ingeniero'),
+    #urls para supervisor
+    path('home/supervisor/', views.home_supervisor, name='home_supervisor'),
+    path('capacitaciones/supervisor/', views.capacitaciones_supervisor, name='capacitaciones_supervisor'),
+    path('evaluaciones/supervisor/', views.evaluaciones_supervisor, name='evaluaciones_supervisor'),
+    path('visitas_medicas/supervisor/', views.visitas_medicas_supervisor, name='visitas_medicas_supervisor'),
+    #urls para tecnico
+    path('home/tecnico/', views.home_tecnico, name='home_tecnico'),
+    path('capacitaciones/tecnico/', views.capacitaciones_tecnico, name='capacitaciones_tecnico'),
+    path('evaluaciones/tecnico/', views.evaluaciones_tecnico, name='evaluaciones_tecnico'),
+    path('visitas_medicas/tecnico/', views.visitas_medicas_tecnico, name='visitas_medicas_tecnico'),
+    #urls para usuarios
+    path('login/usuarios/', views.login, name='login'),
+    path('register/usuarios/', views.register, name='register'),
+
+    
 ]
--- main/views.py
+++ main/views.py
@@ -1,5 +1,63 @@
 from django.shortcuts import render
 
+
 # Create your views here.
-def home(request):
-    return render(request, 'home.html')
+
+#views para cliente
+def home_cliente(request):
+    return render(request, 'cliente/home.html')
+
+def capacitaciones_cliente(request):
+    return render(request, 'cliente/capacitaciones.html')
+
+def mis_evaluaciones_cliente(request):
+    return render(request, 'cliente/mis_evaluaciones.html')
+
+def visitas_medicas_cliente(request):
+    return render(request, 'cliente/visitas_medicas.html')
+
+#views para ingeniero
+def home_ingeniero(request):
+    return render(request, 'ingeniero/home.html')
+
+def capacitaciones_ingeniero(request):
+    return render(request, 'ingeniero/capacitaciones.html')
+
+def evaluaciones_ingeniero(request):
+    return render(request, 'ingeniero/evaluaciones.html')
+
+def visitas_medicas_ingeniero(request):
+    return render(request, 'ingeniero/visitas_medicas.html')
+
+#views para supervisor
+def home_supervisor(request):
+    return render(request, 'supervisor/home.html')
+
+def capacitaciones_supervisor(request):
+    return render(request, 'supervisor/capacitaciones.html')
+
+def evaluaciones_supervisor(request):
+    return render(request, 'supervisor/evaluaciones.html')
+
+def visitas_medicas_supervisor(request):
+    return render(request, 'supervisor/visitas_medicas.html')
+
+#views para tecnico
+def home_tecnico(request):
+    return render(request, 'tecnico/home.html')
+
+def capacitaciones_tecnico(request):
+    return render(request, 'tecnico/capacitaciones.html')
+
+def evaluaciones_tecnico(request):
+    return render(request, 'tecnico/evaluaciones.html')
+
+def visitas_medicas_tecnico(request):
+    return render(request, 'tecnico/visitas_medicas.html')
+
+#views para usuarios
+def login(request):
+    return render(request, 'usuarios/login.html')
+
+def register(request):
+    return render(request, 'usuarios/register.html')
